5ffe8ff72f95897dba05f2e6251bd16565a807f6
Ilaha1996 <ilahaah@code.edu.az>
2024-07-31 00:03:45 +0400

    task uptaded

---
+++ main.js
@@ -0,0 +1,91 @@
+const mazeAPI = "https://api.tvmaze.com/shows";
+let movieDiv = document.getElementById("MovieDiv");
+let searchInput = document.getElementById("searchInput");
+let searchForm = document.getElementById("searchForm");
+let genreSelect = document.getElementById("GenreSelect")
+let movies = [];
+let allGenres =[];
+
+fetch(mazeAPI)
+    .then(response => response.json())
+    .then(data => {
+            movies = data;
+
+            movies.forEach(movie => {
+                movie.genres.forEach(genre => {
+                allGenres.push(genre);
+                    })
+                })
+            
+                let uniqueGenres = allGenres.filter((value, index,array) => array.indexOf(value) ===index).sort();
+
+                CreateOptions(uniqueGenres);              
+                
+                 DisplayMovies(movies);
+                });   
+
+    
+    function CreateOptions(options){
+        options.forEach(uniqueGenre => {
+            let option = document.createElement("option");
+            option.value = uniqueGenre;
+            option.innerText = uniqueGenre;
+            genreSelect.appendChild(option);
+        })
+
+    }
+
+    genreSelect.addEventListener("change", function(){
+        if(genreSelect.value != "")
+            {
+                let filteredByGenres = movies.filter(movie => movie.genres.includes(genreSelect.value))
+                DisplayMovies(filteredByGenres);
+            }
+        else
+        {
+            DisplayMovies(movies);
+        }
+
+    })
+
+        searchForm.addEventListener("submit", function(e){
+            e.preventDefault();
+
+            let movieName = searchInput.value.toLowerCase();
+
+            let filteredMovies = movies.filter(movie => movie.name.toLowerCase().includes(movieName));
+
+            DisplayMovies(filteredMovies);
+
+
+
+        })
+
+
+    function DisplayMovies(movies){
+        movieDiv.innerHTML="";
+        movies.forEach(movie => {
+            movieDiv.innerHTML += `
+                <div class="col-md-3 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="${movie.image.medium}" class="card-img-top" alt="${movie.name}">
+                        <div class="card-body">
+                          <h5 class="card-title">${movie.name}</h5>
+                          <p class="card-text">Premiere: ${movie.premiered}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">Language: ${movie.language}</li>
+                          <li class="list-group-item">Genres: ${movie.genres}</li>
+                          <li class="list-group-item">Rating: ${movie.rating.average}</li>
+                        </ul>
+                        <div class="card-body">
+                            <a href="${movie.officialSite}" class="btn btn-primary" target="_blank">Go to website</a>
+                            <button class="btn btn-success" onclick="window.location.href='second.html?id=${movie.id}'">Go to details</button>
+                        </div>
+                    </div>
+                </div>`;
+
+
+
+        });
+    }
